15ddee0881dfb3c3b1797f5c6e88b62656039087
Understandingvictor <victorflawless7@gmailcom>
2024-02-28 18:09:16 +0100

    Added some files

---
+++ bettyChecks.c
@@ -0,0 +1,14 @@
+/**
+  *main - prints out something
+  *Return: 1 on success
+  */
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	int b = 20;
+
+	printf("The sum of a and b is %d\n", a + b);
+	return (1);
+}
