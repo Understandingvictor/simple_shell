d441bcd48f9024bf112dade6906efe7815797ef0
Understandingvictor <victorflawless7@gmailcom>
2024-02-29 16:01:27 +0100

    made Adjustments

--- commandProcessing.c
+++ commandProcessing.c
@@ -7,15 +7,28 @@
 void process(char *command)
 {
 	char *token;
-	char *args[buffersize];
+	char **args;
 	int status;
 	pid_t pid;
+
+	args = malloc(sizeof(char *) * 2);
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(-1);
+	}
 
 	token = strtok(command, " ");
 	args[0] = token;	
 	args[1] = NULL;
 
 	pid = fork();
+	
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(-1);
+	}
 	if (pid == 0)
 	{
 		if ((execve(args[0], args, NULL)) == -1)
@@ -23,6 +36,10 @@ void process(char *command)
 	}
 	else
 	{
-		wait(&status);
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			exit(1);
+		}
 	}
 }
